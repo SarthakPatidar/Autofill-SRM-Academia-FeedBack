71cfc2e48209453430fbf88f97dfacf38af3cecb
Sarthak <sarthakpatidar@gmail.com>
2018-04-29 12:57:54 +0530

    Add Comments

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ javascript: (function () {
     			for(var j = 2; j <= tr.cells.length; j++) {
     			    if(typeof(tr.querySelectorAll('td li.search-selected-val')[j]) != 'undefined' && tr.querySelectorAll('td li.search-selected-val')[j] != null)
     			    {
-        				tr.querySelectorAll('td li.search-selected-val')[j].setAttribute("selvalue","2727643000027208389");
-        				tr.querySelectorAll('td li.search-selected-val')[j].querySelector("span").innerHTML = "Excellent";
+        				tr.querySelectorAll('td li.search-selected-val')[j].setAttribute("selvalue","2727643000027208389"); // Change Grade Code Here // [Refer to README.md for list of Grade Codes]
+        				tr.querySelectorAll('td li.search-selected-val')[j].querySelector("span").innerHTML = "Excellent"; // Replace Grade Name Here //
         				tr.querySelectorAll('td li.search-selected-val')[j].querySelector("input").setAttribute("value","2727643000027208389");
     			    }
     			}
